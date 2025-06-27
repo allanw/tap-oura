28e2e90c8ff972f0265e5602c8a44def293179e8
Al Whatmough <al.whatmough@gmail.com>
2025-06-27 18:17:11 +1000

    Fix URL param

--- tap_oura/__init__.py
+++ tap_oura/__init__.py
@@ -74,9 +74,9 @@ def do_sync(config, schema):
 
   headers = {"Authorization": f"Bearer {access_token}"}
 
-  resp = requests.get('https://api.ouraring.com/v2/usercollection/sleep?start=%s' % (start_date))
+  resp = requests.get('https://api.ouraring.com/v2/usercollection/sleep?start_date=%s' % (start_date))
 
-  sleeps = resp.json()['sleep']
+  sleeps = resp.json()['data']
 
   for sleep in sleeps:
     singer.write_record('sleeps', sleep)
